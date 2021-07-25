3ab22e2d185769922d96adf67d97d5db25072540
JoharPiaic <joharali365@gmail.com>
2021-07-25 09:52:42 +0500

    changing

--- app.js
+++ app.js
@@ -7,7 +7,46 @@
 
 //object in localstorage
 
-localStorage.setItem("user" , JSON.stringify({name:'Osama'}))
+// localStorage.setItem("user" , JSON.stringify({name:'Osama'}))
 
-localStorage.getItem('user');
+// localStorage.getItem('user');
+
+
+//USERinput
+
+// var maths = document.getElementById('edu');
+// var eng = document.getElementById('eng');
+// var urdu = document.getElementById('urdu');
+
+// var sub = prompt('enter your sub');
+// localStorage.setItem(maths , eng , urdu , JSON.stringify({name:'Osama'}))
+
+// var werson = Person;
+
+// function Person(first, last, age, eye) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eye;
+//   }
+
+//   console.log(Person);
+
+
+// var types = document.getElementById('types');
+// var maths = document.getElementById('edu');
+// var english = document.getElementById('eng');
+// var urdu = document.getElementById('urdu');
+// var submit = document.getElementById('types');
+
+
+// function Person(){
+//     if(types && maths && english && urdu){
+//         console.log(Person)
+//     }
+
+// }
+
+
+
 
