5bfa58c6fb087662db62c71de6faef7b857b1266
JoharPiaic <joharali365@gmail.com>
2021-07-25 09:24:55 +0500

    localStorage

---
+++ app.js
@@ -0,0 +1,13 @@
+//LOCAL STORAGE
+
+// var shedule = {subject:'maths', time:'12pm'}
+
+// localStorage.setItem('shedule', JSON.stringify(shedule));
+
+
+//object in localstorage
+
+localStorage.setItem("user" , JSON.stringify({name:'Osama'}))
+
+localStorage.getItem('user');
+
